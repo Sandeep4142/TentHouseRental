5a484cf7b9d3f528e10a535c08421d28bea17d6d
Sandeep4142 <sandeephoro.sh2019@gmail.com>
2024-03-26 21:04:20 +0530

    updated connection string

--- TentHouseRental/TentHouseRental.DAL/Models/TentHouseRentalContext.cs
+++ TentHouseRental/TentHouseRental.DAL/Models/TentHouseRentalContext.cs
@@ -27,8 +27,6 @@ public partial class TentHouseRentalContext : DbContext
     {
 
     }
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Server=SANDEEPH-WIN10;Database=TentHouseRental;User Id=sa;Password=mindfire; Trust Server Certificate = true");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
